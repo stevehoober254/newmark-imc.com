290907df07dcebf82931301492f8e8fae2946596
WilsonKinyua <wilsonkinyuam@gmail.com>
2022-01-18 12:02:07 +0300

    homepage section done

---
+++ assets/js/scripts.js
@@ -0,0 +1,14 @@
+window.onscroll = function () {
+  myFunction();
+};
+var navbar = document.getElementById("main-header");
+var sticky = navbar.offsetTop;
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+    navbar.style.backgroundColor = "black";
+  } else {
+    navbar.classList.remove("sticky");
+    navbar.style.backgroundColor = "transparent";
+  }
+}
