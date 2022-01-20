cd337f0fd31bbdd7ea8f7242a4d1948271246925
WilsonKinyua <wilsonkinyuam@gmail.com>
2022-01-20 15:47:21 +0300

    about us page done

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,9 +7,17 @@ function myFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky");
     // navbar.style.backgroundColor = "black";
-  } 
+  }
   // else {
   //   navbar.classList.remove("sticky");
   //   navbar.style.backgroundColor = "transparent";
   // }
 }
+
+var vid = document.getElementById("video");
+
+function setPlaySpeed() {
+  vid.playbackRate = 0.5;
+}
+
+setPlaySpeed();
