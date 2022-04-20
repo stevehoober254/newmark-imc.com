dfafbcdc015bf7c742cbfc8f739bb82a8cbed811
WilsonKinyua <wilsonkinyuam@gmail.com>
2022-04-20 19:53:52 +0300

    navigation bar updated

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,18 +1,18 @@
-window.onscroll = function () {
-  myFunction();
-};
-var navbar = document.getElementById("main-header");
-var sticky = navbar.offsetTop;
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky");
-    // navbar.style.backgroundColor = "black";
-  }
-  // else {
-  //   navbar.classList.remove("sticky");
-  //   navbar.style.backgroundColor = "transparent";
-  // }
-}
+// window.onscroll = function () {
+//   myFunction();
+// };
+// var navbar = document.getElementById("main-header");
+// var sticky = navbar.offsetTop;
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky");
+//     // navbar.style.backgroundColor = "black";
+//   }
+//   // else {
+//   //   navbar.classList.remove("sticky");
+//   //   navbar.style.backgroundColor = "transparent";
+//   // }
+// }
 
 // var vid = document.getElementById("video");
 
