598250c3e0b6310311be778bbc12e1a4d7d3830a
WilsonKinyua <wilsonkinyuam@gmail.com>
2022-05-08 18:57:24 +0300

    TESTIMONIALS section

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,39 +1,3 @@
-// window.onscroll = function () {
-//   myFunction();
-// };
-// var navbar = document.getElementById("main-header");
-// var sticky = navbar.offsetTop;
-// function myFunction() {
-//   if (window.pageYOffset >= sticky) {
-//     navbar.classList.add("sticky");
-//     // navbar.style.backgroundColor = "black";
-//   }
-//   // else {
-//   //   navbar.classList.remove("sticky");
-//   //   navbar.style.backgroundColor = "transparent";
-//   // }
-// }
-
-// var vid = document.getElementById("video");
-
-// function setPlaySpeed() {
-//   vid.playbackRate = 0.5;
-// }
-
-// setPlaySpeed();
-
-/* on scroll down hide div with class of 'top header' */
-// var prevScrollpos = window.pageYOffset;
-// window.onscroll = function () {
-//   var currentScrollPos = window.pageYOffset;
-//   if (prevScrollpos > currentScrollPos) {
-//     document.querySelector(".top-header-contact").style.display = "none";
-//   } else {
-//     document.querySelector(".top-header-contact").style.display = "block";
-//   }
-//   prevScrollpos = currentScrollPos;
-// };
-
 $(document).ready(function () {
   $(".owl-carousel").owlCarousel({
     autoplay: true,
