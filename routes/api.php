a88a0005a322ae2793b7562a4241dfb295928931
WilsonKinyua <wilsonkinyuam@gmail.com>
2022-05-22 00:28:33 +0300

    homepage section done

---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+Route::group(['prefix' => 'v1', 'as' => 'api.', 'namespace' => 'Api\V1\Admin', 'middleware' => ['auth:sanctum']], function () {
+    // Users
+    Route::post('users/media', 'UsersApiController@storeMedia')->name('users.storeMedia');
+    Route::apiResource('users', 'UsersApiController');
+
+    // Team
+    Route::apiResource('teams', 'TeamApiController');
+});
