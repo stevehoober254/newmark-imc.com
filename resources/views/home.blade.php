f75688264571389c8349cff7fc892b119d6a8c86
WilsonKinyua <wilsonkinyuam@gmail.com>
2022-06-04 21:13:34 +0300

    homepage updated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,6 @@
                 <div class="card-header">
                     Dashboard
                 </div>
-
                 <div class="card-body">
                     @if(session('status'))
                         <div class="alert alert-success" role="alert">
