e43cabafe4eefc5972437eb6f19d4666743bc901
Wilson Kinyua <wilsonkinyuam@gmail.com>
2022-11-15 15:36:58 +0300

    siderbar updated

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -62,16 +62,6 @@
                                 {{ trans('global.login') }}
                             </button>
                         </div>
-                        <div class="col-6 text-right">
-                            @if(Route::has('password.request'))
-                                <a class="btn btn-link px-0" href="{{ route('password.request') }}">
-                                    {{ trans('global.forgot_password') }}
-                                </a><br>
-                            @endif
-                            <a class="btn btn-link px-0" href="{{ route('register') }}">
-                                {{ trans('global.register') }}
-                            </a>
-                        </div>
                     </div>
                 </form>
             </div>
